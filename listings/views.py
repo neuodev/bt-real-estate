8066db09ba5eee001e0063d7f77b187156e7d2ff
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-16 11:33:10 +0200

    Create the view metods

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(req):
+    render(req, 'listings/listings.html')
+
+def listing(req):
+    render(req, 'listings/listing.html')
+
+def search(req):
+    render(req, 'listings/search.html')
