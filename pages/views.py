ed79c4920d6330ae6a6beb496d982322535b9ae0
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-16 03:19:10 +0200

    Add the the about function to render the about page

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse('<h1>hello world</h1>')
+    return render(request,'pages.index.html')
+def about(request):
+    return render(request, 'pages.about')
