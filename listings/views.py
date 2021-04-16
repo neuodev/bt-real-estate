33ee5917754ac9690cce6897e84d2d489260b5fb
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-16 11:44:33 +0200

    test the urls

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def index(req):
-    render(req, 'listings/listings.html')
+   return render(req, 'listings/listings.html')
 
 def listing(req):
-    render(req, 'listings/listing.html')
+    return render(req, 'listings/listing.html')
 
 def search(req):
-    render(req, 'listings/search.html')
+    return render(req, 'listings/search.html')
