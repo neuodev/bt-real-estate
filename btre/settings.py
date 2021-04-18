c332e1418beba1bdd290010e6117e1428e01ecdc
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-18 14:03:36 +0200

    configer the setting to accept media

--- btre/settings.py
+++ btre/settings.py
@@ -134,3 +134,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#  Media Folder Settings 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
