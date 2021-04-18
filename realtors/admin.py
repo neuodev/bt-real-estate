72ff732fb12ed3dcc1ea2623a5880b1f27517520
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-18 15:31:39 +0200

    cusomize the amdin area for teh realtor

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','hire_date' , 'is_mvp')
+    list_display_links = ('id' ,'name')
+    list_editable = ('is_mvp',)
+    search_fields = ('name' , 'email')
+    list_per_page = 20
+
+
+admin.site.register(Realtor, RealtorAdmin)
