f603046718d5486b9fc9d62d6c529a135f682971
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-18 20:13:50 +0200

    Fetch listing from the database

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+
 def index(request):
-    return render(request,'pages/index.html')
+    # get the latest Listings
+    listings = Listing.objects.order_by('-list_date')[:3]
+    # pass it to the context 
+    context = {'listings':listings}
+    # pass the contect to teh render 
+    return render(request,'pages/index.html' , context)
    
 
 def about(request):
