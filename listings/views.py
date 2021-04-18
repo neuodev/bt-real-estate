50f5d4c62b326129993043e998a6d7ad59f436ab
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-18 23:19:28 +0200

    add filter

--- listings/views.py
+++ listings/views.py
@@ -31,11 +31,31 @@ def listing(req ,listing_id):
 def search(req):
     # get all listings
     query_listings = Listing.objects.order_by('-list_date')
-    if 'keywords' in req.GET['keywords']:
+    # keywords
+    if 'keywords' in req.GET:
         keywords = req.GET['keywords']
         if keywords:
-            query_listings.filter(description__icontains=keywords)
-
+           query_listings= query_listings.filter(description__icontains=keywords)
+    # city 
+    if 'city' in req.GET:
+        city = req.GET['city']
+        if city: 
+            query_listings = query_listings.filter(city__iexact=city)
+    # price 
+    if 'price' in req.GET:
+        price = req.GET['price']
+        if price: 
+            query_listings = query_listings.filter(price__lte=int(price))
+    # bedrooms 
+    if 'bedrooms' in req.GET:
+        bedrooms = req.GET['bedrooms']
+        if bedrooms: 
+            query_listings = query_listings.filter(bedrooms__lte=int(bedrooms))
+    # state 
+    if 'state' in req.GET:
+        state = req.GET['state']
+        if state: 
+            query_listings = query_listings.filter(state__iexact=state)
     context = {
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
