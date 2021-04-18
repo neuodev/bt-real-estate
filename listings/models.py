bbb13cd330051cc4b65c3f2e19fca4688c91bbdc
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-18 13:40:57 +0200

    Finish the Realtors model

--- listings/models.py
+++ listings/models.py
@@ -23,6 +23,6 @@ class Listing(models.Model):
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateField(default=datetime.now , blank=True)
+    list_date = models.DateTimeField(default=datetime.now , blank=True)
     def __str__(self):
         return self.title
