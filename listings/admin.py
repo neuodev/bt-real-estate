3a35e0c1dabc7054b46ba1972cdd1db1b5cfede2
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-18 15:22:32 +0200

    Customize the admin for listing by adding filter , sorting, ispublised

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin (admin.ModelAdmin):
+    list_display = ('id','title','is_published','price','list_date', 'realtor')
+    list_display_links = ('id','title')
+    list_filter = ('realtor', 'is_published')
+    list_editable = ('is_published',)
+
+admin.site.register(Listing ,ListingAdmin)
